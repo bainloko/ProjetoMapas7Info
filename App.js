36f3ea59a7d5d52cbbd6e69fb32ee310139c8203
Kaua <kaua.cousillas03@gmail.com>
2022-03-23 00:59:11 -0300

    arquivo recuperado da minha memória... aula 22/03

--- App.js
+++ App.js
@@ -9,11 +9,25 @@ import MapView from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <MapView style={styles.map} />
-    </View>
-  );
+    var latitude = -31.33, longitude = -54.07;
+
+    return (
+        <View style={styles.container}>
+            <MapView
+                provider={"google"}
+                initialRegion={{latitude: latitude, longitude: longitude, latitudeDelta: 0.0, latitudeDelta: 0.0}}
+                mapType="hybrid"
+                loadingEnabled={true}
+                style={styles.map}>
+
+                <MapView.Marker
+                    coordinate={{latitude: latitude, longitude: longitude}}
+                    title={"Este é um Marcador!"}
+                    description={"Esta é a descrição padrão deste Marcador."}
+                />
+            </MapView>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -22,6 +36,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  bar: {
+    height: Dimensions.get('window').height * .05,
+    backgroundColor: '#fff',
   },
   map: {
     width: Dimensions.get('window').width,
